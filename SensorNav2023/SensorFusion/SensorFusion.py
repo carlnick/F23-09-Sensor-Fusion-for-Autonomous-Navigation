67e35aa730a341ea622f68a1108ef96759e2fd00
carlnick <carln@vt.edu>
2023-08-23 14:10:01 -0400

    file description comment to test git push

---
+++ SensorNav2023/SensorFusion/SensorFusion.py
@@ -0,0 +1,10 @@
+##################################################
+# This is the main file for the Sensor Fusion Algorithm 
+# 
+# main at bottom, worker classes above
+# 
+# Authors: Marlin Spears, Carl Nicklas
+# 
+###################################################
+
+import numpy
