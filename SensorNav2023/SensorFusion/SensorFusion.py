39237cdc24223b35ebe6816542f0cf8c733e96cf
Marlin Spears <mlspears1228@vt.edu>
2023-08-30 15:05:45 -0400

    Combined Vector Class into file

--- SensorNav2023/SensorFusion/SensorFusion.py
+++ SensorNav2023/SensorFusion/SensorFusion.py
@@ -14,6 +14,16 @@ import adafruit_mmc56x3
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 import random
 
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+# Placeholder values, currently unknown what should be within these
+threshaccel = Vector(0.0, 0.0, 0.0)
+threshgyro = Vector(0.0, 0.0, 0.0)
+
 def threshold_mag(data, last_data):
     # Assuming  data is in microTeslas
     if data < -100 or data > 100:
@@ -24,6 +34,24 @@ def threshold_mag(data, last_data):
             return "data bad"
         
     return "data good"
+
+# Compare the acceleromter and gyroscope values to their thresholds and sets their values to the previous accelerometer and gyroscope vectors
+def IMUthreshold(accelerometer, gyroscope, accel, gyro):
+    if accelerometer.x <= threshaccel.x:
+        accel.x = accelerometer.x
+    if accelerometer.y <= threshaccel.y:
+        accel.y = accelerometer.y
+    if accelerometer.z <= threshaccel.z:
+        accel.z = accelerometer.z
+
+    if gyroscope.x <= threshgyro.x:
+        gyro.x = gyroscope.x
+    if gyroscope.y <= threshgyro.y:
+        gyro.y = gyroscope.y
+    if gyroscope.z <= threshgyro.z:
+        gyro.z = gyroscope.z
+
+    return accel, gyro
     
 
 if __name__ == "__main__":
@@ -33,10 +61,16 @@ if __name__ == "__main__":
     # i2c_mag = board.I2C()
     # sensor_mag = adafruit_mmc56x3.MMC5603(i2c_mag)
 
+    accelerometer = Vector(sensor_imu.acceleration)
+    gyroscope = Vector(sensor_imu.gyro)
+    magnetometer = Vector(sensor_mag.magnetic)
+
     # Time propagation tracking variables
     last_mag_reading: float = -55555
-    last_acc_reading: float = -55555
-    last_gyr_reading: float = -55555
+    # last_acc_reading: float = -55555
+    # last_gyr_reading: float = -55555
+    last_acc_reading = Vector(0.0, 0.0, 0.0)
+    last_gyr_reading = Vector(0.0, 0.0, 0.0)
     
     current_mag = 0
     #looped code
@@ -47,4 +81,6 @@ if __name__ == "__main__":
         # print(sensor_imu.gyro)
         last_mag_reading = current_mag
         current_mag += random.random()*5
+        last_acc_reading, last_gyr_reading = IMUthreshold(accelerometer, gyroscope, last_acc_reading, last_gyr_reading)
+        # get the new acceleration and gyro readings
         time.sleep(1.0)
