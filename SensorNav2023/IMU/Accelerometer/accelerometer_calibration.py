2d18f67a71564bca89cccb999c7b296db0d4c19f
Adam Lahouar <adamlahouar@vt.edu>
2023-09-20 17:37:46 -0400

    Added accelerometer calibration and voting functions

---
+++ SensorNav2023/IMU/Accelerometer/accelerometer_calibration.py
@@ -0,0 +1,84 @@
+import time
+import board
+import adafruit_tca9548a
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+
+i2c = board.I2C()
+
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+imu0 = LSM6DSOX(tca[0])
+imu1 = LSM6DSOX(tca[3])
+imu2 = LSM6DSOX(tca[5])
+
+
+def collect_data(imu, axis):
+    num_samples = 100
+    data_average = 0.0
+    timestep = 0.1
+    for i in range(num_samples):
+        data_average += imu.acceleration[axis]
+        time.sleep(timestep)
+
+    return data_average / num_samples
+
+
+imu0_data = [[0] * 3] * 3
+imu1_data = [[0] * 3] * 3
+imu2_data = [[0] * 3] * 3
+
+input("Orient system upright and press enter to continue")
+print("Collecting data")
+
+imu0_data[0][1] = collect_data(imu0, 0)
+imu0_data[1][1] = collect_data(imu0, 1)
+imu0_data[2][0] = collect_data(imu0, 2)
+
+imu1_data[0][1] = collect_data(imu1, 0)
+imu1_data[1][1] = collect_data(imu1, 1)
+imu1_data[2][0] = collect_data(imu1, 2)
+
+imu2_data[0][1] = collect_data(imu2, 0)
+imu2_data[1][1] = collect_data(imu2, 1)
+imu2_data[2][0] = collect_data(imu2, 2)
+
+input("Orient system with X-axis up and press enter to continue")
+
+imu0_data[0][0] = collect_data(imu0, 0)
+imu0_data[2][1] = collect_data(imu0, 2)
+
+imu1_data[0][0] = collect_data(imu1, 0)
+imu1_data[2][1] = collect_data(imu1, 2)
+
+imu2_data[0][0] = collect_data(imu2, 0)
+imu2_data[2][1] = collect_data(imu2, 2)
+
+input("Orient system with Z-axis down and press enter to continue")
+
+imu0_data[2][2] = collect_data(imu0, 2)
+imu1_data[2][2] = collect_data(imu1, 2)
+imu2_data[2][2] = collect_data(imu2, 2)
+
+input("Orient system with X-axis down and press enter to continue")
+
+imu0_data[0][2] = collect_data(imu0, 0)
+imu1_data[0][2] = collect_data(imu1, 0)
+imu2_data[0][2] = collect_data(imu2, 0)
+
+input("Orient system with Y-axis up and press enter to continue")
+
+imu0_data[1][0] = collect_data(imu0, 1)
+imu1_data[1][0] = collect_data(imu1, 1)
+imu2_data[1][0] = collect_data(imu2, 1)
+
+input("Orient system with Y-axis down and press enter to continue")
+
+imu0_data[1][2] = collect_data(imu0, 1)
+imu1_data[1][2] = collect_data(imu1, 1)
+imu2_data[1][2] = collect_data(imu2, 1)
+
+print("Calibration complete")
+
+print("IMU0 Data:", imu0_data)
+print("IMU1 Data:", imu1_data)
+print("IMU2 Data:", imu2_data)
--- SensorNav2023/IMU/vote.py
+++ SensorNav2023/IMU/vote.py
@@ -1,19 +1,19 @@
 # This function takes three values (sensor measurements) and returns the two that are closer together
-# Written by Adam Lahouar
-def vote(val1, val2, val3):
+# Written by Adam Lahouar (09/07/23)
+def vote(a, b, c):
     # Calculate absolute differences
-    diff_val1_val2 = abs(val1 - val2)
-    diff_val1_val3 = abs(val1 - val3)
-    diff_val2_val3 = abs(val2 - val3)
+    diff_ab = abs(a - b)
+    diff_ac = abs(a - c)
+    diff_bc = abs(b - c)
 
     # Find the minimum difference
-    min_diff = min(diff_val1_val2, diff_val1_val3, diff_val2_val3)
+    min_diff = min(diff_ab, diff_ac, diff_bc)
 
     # Map differences to value pairs
     diff_pair_mapping = {
-        diff_val1_val2: (val1, val2),
-        diff_val1_val3: (val1, val3),
-        diff_val2_val3: (val2, val3)
+        diff_ab: (a, b),
+        diff_ac: (a, c),
+        diff_bc: (b, c)
     }
 
     # Return the value pair corresponding to the minimum difference
