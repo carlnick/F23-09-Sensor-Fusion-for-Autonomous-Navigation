0367f60027ffac35b3b2893384c361d2cab13cbe
Adam Lahouar <adamlahouar@vt.edu>
2023-10-03 14:00:48 -0400

    Updated gyroscope calibration code to calibrate all axes back to back

--- SensorNav2023/IMU/Gyroscope/gyroscope_calibration.py
+++ SensorNav2023/IMU/Gyroscope/gyroscope_calibration.py
@@ -1,85 +1,60 @@
-# from pynput import keyboard
-# import numpy as np
-# import time
-# import board
-# import adafruit_tca9548a
-# from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
-#
-# i2c = board.I2C()
-#
-# tca = adafruit_tca9548a.TCA9548A(i2c)
-#
-# imu0 = LSM6DSOX(tca[0])
-# imu1 = LSM6DSOX(tca[3])
-# imu2 = LSM6DSOX(tca[5])
-#
-#
-# def on_key_release(key):
-#     return key == keyboard.Key.enter
-#
-#
-# def collect_rotate_integrate(imu, axis):
-#     data = []
-#     timestep = 0.1
-#
-#     with keyboard.Listener(on_release=on_key_release) as listener:
-#         listener.join()
-#
-#     while True:
-#         data.append(imu.gyro[axis])
-#         time.sleep(timestep)
-#
-#     # integrate data using cumtrapz
-#     data_integrated = np.cumtrapz(data, dx=timestep)
-#     return 360.0 / data_integrated
-#
-#
-# input("Orient system upright, press enter, rotate 360 degrees, and press enter again")
-# print("Collecting data")
-# print(collect_rotate_integrate(imu0, 0))
-
-import keyboard
+from pynput import keyboard
 import time
 import board
 import adafruit_tca9548a
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
-import threading
+import numpy as np
+
+timestep = 0.01
 
 
 def initialize_sensors():
     i2c = board.I2C()
     tca = adafruit_tca9548a.TCA9548A(i2c)
-    imu0 = LSM6DSOX(tca[0])
-    imu1 = LSM6DSOX(tca[3])
-    imu2 = LSM6DSOX(tca[5])
+    imu0 = LSM6DSOX(tca[1])
+    imu1 = LSM6DSOX(tca[4])
+    imu2 = LSM6DSOX(tca[7])
     return imu0, imu1, imu2
 
 
-def collect_data(imu, axis):
-    raw_data = []
-    timestep = 0.01
-    while True:
-        if keyboard.is_pressed("e"):
-            break
-        raw_data.append(imu.gyro[axis])
-        time.sleep(timestep)
+def on_key_press(key):
+    print("ENTER PRESSED")
+    if key == keyboard.Key.enter:
+        return False
 
-    return raw_data
+
+def collect_data(axis, imus):
+    imu0_data = []
+    imu1_data = []
+    imu2_data = []
+
+    with keyboard.Listener(on_press=on_key_press) as listener:
+        while True:
+            if not listener.running:
+                break
+            imu0_data.append(imus[0].gyro[axis])
+            imu1_data.append(imus[1].gyro[axis])
+            imu2_data.append(imus[2].gyro[axis])
+            time.sleep(timestep)
+
+    return [imu0_data, imu1_data, imu2_data]
 
 
 if __name__ == "__main__":
-    imu0, imu1, imu2 = initialize_sensors()
-    try:
-        print("press e, rotate 360 around X-axis, and press e again to stop data collection")
-        thread1 = threading.Thread(target=collect_data, args=(imu0, 0))
-        thread2 = threading.Thread(target=collect_data, args=(imu1, 0))
-        thread3 = threading.Thread(target=collect_data, args=(imu2, 0))
-        thread1.start()
-        thread2.start()
-        thread3.start()
-    except KeyboardInterrupt:
-        print("Keyboard interrupt detected. Exiting the program.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
 
-    keyboard.unhook_all()
+    sensors = initialize_sensors()
+
+    for axis in range(0, 3):
+        data = collect_data(axis, sensors)
+        imu0_rotation = np.rad2deg(np.trapz(data[0], dx=timestep))
+        imu1_rotation = np.rad2deg(np.trapz(data[1], dx=timestep))
+        imu2_rotation = np.rad2deg(np.trapz(data[2], dx=timestep))
+
+        print(f"IMU 0 rotation in axis {axis}: {imu0_rotation} degrees")
+        print(f"IMU 1 rotation in axis {axis}: {imu1_rotation} degrees")
+        print(f"IMU 2 rotation in axis {axis}: {imu2_rotation} degrees")
+        if axis == 2:
+            break
+        print("Prepare to start rotating around next axis")
+        time.sleep(10)
+        print("Start rotating")
