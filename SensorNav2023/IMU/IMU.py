bd029bdefc2c8c46ffa8eb79d4f020d9e915783d
Adam Lahouar <adamlahouar@vt.edu>
2023-10-11 22:35:30 -0400

    Added methods to get sensor data in all axes with one call, separate methods for accelerometer and gyroscope

--- SensorNav2023/IMU/IMU.py
+++ SensorNav2023/IMU/IMU.py
@@ -1,100 +1,52 @@
 import board
 import adafruit_tca9548a
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
-from typing import Literal
-
-# ACM is the accelerometer calibration matrix, to be filled in with the calibration data
-ACM = [[[0.0, 1.0]] * 3] * 3
-
-# GCM is the gyroscope calibration matrix, to be filled in with the calibration data
-GCM = [[[0.0, 1.0]] * 3] * 3
-
-# Multiplexer port numbers
-allowed_ports = Literal[0, 1, 2, 3, 4, 5, 6, 7]
-
-
-def _vote_and_average(a: float, b: float, c: float) -> float:
-    # Calculate absolute differences
-    diff_ab = abs(a - b)
-    diff_ac = abs(a - c)
-    diff_bc = abs(b - c)
-
-    # Find the minimum difference
-    min_diff = min(diff_ab, diff_ac, diff_bc)
-
-    # Map differences to value pairs
-    diff_pair_mapping = {
-        diff_ab: (a, b),
-        diff_ac: (a, c),
-        diff_bc: (b, c)
-    }
-
-    # Return the average of the value pair corresponding to the minimum difference
-    best_values = diff_pair_mapping[min_diff]
-    return (best_values[0] + best_values[1]) / 2.0
 
 
 class IMU:
-    def __init__(self, imu_ports: list[allowed_ports]):
-        """
+    def __init__(self, imu0_port: int, imu1_port: int, imu2_port: int):
+        i2c = board.I2C()
+        tca = adafruit_tca9548a.TCA9548A(i2c)
+        self.imu0 = LSM6DSOX(tca[imu0_port])
+        self.imu1 = LSM6DSOX(tca[imu1_port])
+        self.imu2 = LSM6DSOX(tca[imu2_port])
+        self.accelerometer_calibration_matrix = [[[0.0] * 2] * 3] * 3
+        self.gyroscope_calibration_matrix = [[[0.0] * 2] * 3] * 3
 
-        :param imu_ports: A list of ports that the IMUs are connected to, e.g. [2, 5, 6]
-        """
-        mux = adafruit_tca9548a.TCA9548A(board.I2C())
+    def _vote(self, a: float, b: float, c: float) -> float:
+        # Calculate absolute differences
+        diff_ab = abs(a - b)
+        diff_ac = abs(a - c)
+        diff_bc = abs(b - c)
 
-        self.IMU0 = LSM6DSOX(mux[imu_ports[0]])
-        self.IMU1 = LSM6DSOX(mux[imu_ports[1]])
-        self.IMU2 = LSM6DSOX(mux[imu_ports[2]])
+        # Find the minimum difference
+        min_diff = min(diff_ab, diff_ac, diff_bc)
 
-    def _get_sensor_data(self, axis: str, raw_data, CM) -> float:
-        """
+        # Map differences to value pairs
+        diff_pair_mapping = {
+            diff_ab: (a, b),
+            diff_ac: (a, c),
+            diff_bc: (b, c)
+        }
 
-
-        :param axis: The axis to get data from
-        :param raw_data: The raw data function that gets the data from the sensor and axis
-        :param CM: The calibration matrix of the sensor
-        :return: A calibrated, filtered, and averaged value from the sensor and axis
-        """
-
-        # Check that the axis is valid
-        if len(axis) != 1:
-            raise ValueError("Axis must be a single character (x, y, or z)")
-
-        axis_number = ord(axis) - ord('x')
-
-        if axis_number < 0 or axis_number > 2:
-            raise ValueError("Axis must be x, y, or z")
-
-        # Get the raw data as a list
-        raw_data = [raw_data(self.IMU0, axis_number),
-                    raw_data(self.IMU1, axis_number),
-                    raw_data(self.IMU2, axis_number)]
-
-        # f(x) = (x + b) * s
-        # x is the raw data, b is the bias offset, and s is the scale factor
-        # Apply the calibration matrix to the raw data
-        calibrated_data = [(raw_data[0] + CM[0][axis_number][0]) * CM[0][axis_number][1],
-                           (raw_data[1] + CM[1][axis_number][0]) * CM[1][axis_number][1],
-                           (raw_data[2] + CM[2][axis_number][0]) * CM[2][axis_number][1]]
-
-        return _vote_and_average(calibrated_data[0], calibrated_data[1], calibrated_data[2])
+        # Return the value pair corresponding to the minimum difference
+        return diff_pair_mapping[min_diff]
 
     def get_acceleration(self, axis: str) -> float:
-        """
-        Uses a lambda function to get the raw data from the sensor and axis and passes it to the _get_sensor_data
-        method to do the calibration, filtering, and averaging
+        axis_num = ord(axis) - ord('x')
 
-        :param axis: The axis to get data from
-        :return: The calibrated, filtered, and averaged linear acceleration in meters per second^2 in the selected axis
-        """
-        return self._get_sensor_data(axis, lambda imu, axis_num: imu.acceleration[axis_num], ACM)
+        return self._vote(self.imu0.acceleration[axis_num],
+                          self.imu1.acceleration[axis_num],
+                          self.imu2.acceleration[axis_num])
 
-    def get_gyroscope(self, axis: str) -> float:
-        """
-        Uses a lambda function to get the raw data from the sensor and axis and passes it to the _get_sensor_data
-        method to do the calibration, filtering, and averaging
+    def get_gyro(self, axis: str) -> float:
+        axis_num = ord(axis) - ord('x')
+        return self._vote(self.imu0.gyro[axis_num],
+                          self.imu1.gyro[axis_num],
+                          self.imu2.gyro[axis_num])
 
-        :param axis: The axis to get data from
-        :return: The calibrated, filtered, and averaged angular velocity in degrees per second in the selected axis
-        """
-        return self._get_sensor_data(axis, lambda imu, axis_num: imu.gyro[axis_num], GCM)
+    def get_acceleration_all(self) -> list:
+        return [self.get_acceleration(axis) for axis in "xyz"]
+
+    def get_gyro_all(self) -> list:
+        return [self.get_gyro(axis) for axis in "xyz"]
