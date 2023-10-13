a1fc5450be26c52a003ac2604907c4d936ab357f
Hayden <haywillcra@gmail.com>
2023-10-13 15:13:17 -0400

    prototype working

--- SensorNav2023/main.py
+++ SensorNav2023/main.py
@@ -4,7 +4,9 @@ from SensorFusion.ComplementaryFilter import ComplementaryFilter
 from SensorFusion.SensorFusion import orientation 
 from IMU.IMU import IMU
 from Magnetometer.Magnetometer import Magnetometer
-
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 # MAIN PROGRAM
 if __name__ == "__main__":
 
@@ -33,7 +35,72 @@ if __name__ == "__main__":
     compFilter = ComplementaryFilter(qFirstPrediction)
 
     while(True):
+        # Create figure for plotting
+        # fig = plt.figure()
+        # ar = fig.add_subplot(3, 1, 1)
+        # ap = fig.add_subplot(3, 1, 2)
+        # ay = fig.add_subplot(3, 1, 3)
+        # xs = []
+        # ys_r = []
+        # ys_p = []
+        # ys_y = []
 
+        # # This function is called periodically from FuncAnimation
+        # def animate(i, xs, ys_r, ys_p, ys_y):
+        #     # Obtain sensor data
+        #     vAccelerometer.x = imu.get_acceleration('x')
+        #     vAccelerometer.y = imu.get_acceleration('y')
+        #     vAccelerometer.z = imu.get_acceleration('z')
+
+        #     vMagnetometer.set(*mag.get_magnetic())
+
+        #     vGyroscope.x = imu.get_gyroscope('x')
+        #     vGyroscope.y = imu.get_gyroscope('y')
+        #     vGyroscope.z = imu.get_gyroscope('z')
+
+        #     # Normalize vectors for orientation sensor fusion
+        #     vNormAccel = vAccelerometer.normalize()
+        #     vNormMag = vMagnetometer.normalize()
+
+        #     # Prediction Step
+        #     compFilter.predict(vGyroscope)
+
+        #     # Correction Step
+        #     compFilter.correctOrientation(vNormAccel, vNormMag)
+        #     # Obtain Corrected Orientation
+        #     print(compFilter.toEuler(compFilter.qResult))
+        #     roll_pitch_yaw:Vector = compFilter.toEuler(compFilter.qResult)
+        #     # print(compFilter.qResult)
+        #     # Add x and y to lists
+        #     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+        #     ys_r.append(roll_pitch_yaw.x)
+        #     ys_p.append(roll_pitch_yaw.y)
+        #     ys_y.append(roll_pitch_yaw.z)
+
+        #     # Limit x and y lists to 20 items
+        #     xs = xs[-20:]
+        #     ys_r = ys_r[-20:]
+        #     ys_p = ys_p[-20:]
+        #     ys_y = ys_y[-20:]
+
+        #     # Draw x and y lists
+        #     ar.clear()
+        #     ar.plot(xs, ys_r)
+        #     ap.clear()
+        #     ap.plot(xs, ys_p)
+        #     ay.clear()
+        #     ay.plot(xs, ys_y)
+
+        #     # Format plot
+        #     plt.xticks(rotation=45, ha='right')
+        #     plt.subplots_adjust(bottom=0.30)
+        #     plt.title('Euler Angles over Time')
+        #     plt.ylabel('Top to bottom: Roll, Pitch, Yaw')
+        #     return
+
+        # # Set up plot to call animate() function periodically
+        # ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys_r, ys_p, ys_y), interval=100)
+        # plt.show()
         # Obtain sensor data
         vAccelerometer.x = imu.get_acceleration('x')
         vAccelerometer.y = imu.get_acceleration('y')
@@ -56,4 +123,14 @@ if __name__ == "__main__":
         compFilter.correctOrientation(vNormAccel, vNormMag)
         # Obtain Corrected Orientation
         # compFilter.graphResult()
-        print(compFilter.qResult)
+        
+        
+        euler = compFilter.toEuler(compFilter.qResult)
+        roll = round(euler.x, 2)
+        pitch = round(euler.y, 2)
+        yaw = round(euler.z, 2)
+        
+        print(f"Roll: {roll}, Pitch: {pitch}, Yaw: {yaw}")
+        
+        # print(compFilter.toEuler(compFilter.qResult))
+        # print(compFilter.qResult)
